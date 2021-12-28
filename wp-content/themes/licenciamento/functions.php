6f261072c7c2a224a7c1d7d8beeb98257594552a
vgiuliani11 <vinicius.giuliani@gmail.com>
2021-12-28 18:45:23 -0300

    refactor: remoção de assets do projeto, css movido para a raiz

--- wp-content/themes/licenciamento/functions.php
+++ wp-content/themes/licenciamento/functions.php
@@ -40,8 +40,9 @@ if ( ! function_exists( 'lct_theme_setup' ) ) {
 function lct_load_scripts() {
 
 	wp_enqueue_style( 'lct-base-style', get_stylesheet_uri(), array(), '1.0' );
-	wp_enqueue_style( 'acessivel', get_template_directory_uri() . '/css/acessivel.css', false, '1.0', 'all');
-	wp_enqueue_style( 'default', get_template_directory_uri() . '/css/default.css', false, '1.0.2', 'all');
+	wp_enqueue_style( 'acessivel', '/css/acessivel.css', false, '1.0', 'all');
+	wp_enqueue_style( 'default', '/css/default.css', false, '1.2.0', 'all');
+	wp_enqueue_style( 'paginas_ajuda', '/css/ajuda.css', false, '1.1', 'all');
 	wp_enqueue_script( 'jquery' );
 
 } add_action( 'wp_enqueue_scripts', 'lct_load_scripts' );
