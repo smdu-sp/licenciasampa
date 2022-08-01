8ed18df6b1b4f2eabfdb1e466d91b51d4d9d6c07
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-08-01 18:15:53 -0300

    Ajustes de locais/diretórios do projeto

--- wp-content/themes/licenciamento/header.php
+++ wp-content/themes/licenciamento/header.php
@@ -17,8 +17,8 @@
 
 	<title><?php lct_title('|'); ?></title>
 
-	<link rel="icon" href="assets/favicon.ico" type="image/x-icon" />
-	<link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon" />
 
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
