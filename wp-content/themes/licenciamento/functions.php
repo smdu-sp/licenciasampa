5f03f36da3adb1cdc4e82fc9daf1858c30a26d21
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-08-11 17:38:59 -0300

    CSS: Seção de avisos e correção do responsivo do menu nav

--- wp-content/themes/licenciamento/functions.php
+++ wp-content/themes/licenciamento/functions.php
@@ -2,7 +2,9 @@
 /**
  * Functions and definitions.
  *
- * @package AprovaSP
+ * @link https://livecomposerplugin.com/themes/
+ *
+ * @package LC Blank
  */
 
 // Delcare Header/Footer compatibility.
@@ -39,8 +41,8 @@ function lct_load_scripts() {
 
 	wp_enqueue_style( 'lct-base-style', get_stylesheet_uri(), array(), '1.0' );
 	wp_enqueue_style( 'acessivel', '/css/acessivel.css', false, '1.0', 'all');
-	wp_enqueue_style( 'default', '/css/default.css', false, '1.5', 'all');
-	wp_enqueue_style( 'menus', '/css/menus.css', false, '1.0', 'all');
+	wp_enqueue_style( 'default', '/css/default.css', false, '1.6', 'all');
+	wp_enqueue_style( 'menus', '/css/menus.css', false, '1.1', 'all');
 	wp_enqueue_style( 'botoes', '/css/botoes.css', false, '1.0.2', 'all');
 	wp_enqueue_style( 'paginas_ajuda', '/css/ajuda.css', false, '1.1', 'all');
 	wp_enqueue_style( 'breadcrumb', '/css/breadcrumb.css', false, '1.0', 'all');
