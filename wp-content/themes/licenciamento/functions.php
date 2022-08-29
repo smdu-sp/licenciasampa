5e6e8f7144afe810378be1df896021598249f756
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-08-29 17:27:01 -0300

    Funções de acesso ao DB e largura do grid

--- wp-content/themes/licenciamento/functions.php
+++ wp-content/themes/licenciamento/functions.php
@@ -37,18 +37,32 @@ if ( ! function_exists( 'lct_theme_setup' ) ) {
 /**
  * Load JS and CSS files.
  */
-function lct_load_scripts() {
-
+function lct_load_scripts()
+{
+	// Bootstrap
+	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css', false, '5.2.0', 'all');
+	
+	// Estilo Padrão
 	wp_enqueue_style( 'lct-base-style', get_stylesheet_uri(), array(), '1.0' );
+	
+	// Dashicons
+	wp_enqueue_style( 'dashicons' );
+	
+	// Personalização
 	wp_enqueue_style( 'acessivel', '/css/acessivel.css', false, '1.0', 'all');
-	wp_enqueue_style( 'default', '/css/default.css', false, '1.6', 'all');
-	wp_enqueue_style( 'menus', '/css/menus.css', false, '1.1.0', 'all');
-	wp_enqueue_style( 'botoes', '/css/botoes.css', false, '1.0.2', 'all');
+	wp_enqueue_style( 'default', '/css/default.css', false, '1.7', 'all');
+	wp_enqueue_style( 'menus', '/css/menus.css', false, '1.2', 'all');
+	wp_enqueue_style( 'botoes', '/css/botoes.css', false, '1.1', 'all');
 	wp_enqueue_style( 'paginas_ajuda', '/css/ajuda.css', false, '1.1', 'all');
 	wp_enqueue_style( 'breadcrumb', '/css/breadcrumb.css', false, '1.0', 'all');
-	wp_enqueue_script( 'jquery' );
+	wp_enqueue_style( 'cadastro', '/css/cadastro.css', false, '1.0', 'all');
 
-} add_action( 'wp_enqueue_scripts', 'lct_load_scripts' );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'lct_load_scripts' );
 
 if ( ! defined( 'DS_LIVE_COMPOSER_VER' ) ) {
 
@@ -73,10 +87,47 @@ function lct_title( $sep ) {
 	bloginfo( 'name ' );
 }
 
-//Enqueue the Dashicons script
-add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
-function load_dashicons_front_end() {
-wp_enqueue_style( 'dashicons' );
+// Query no DB
+function getDados(string $tabela, array $condicoes = [])
+{
+	global $wpdb;
+	$where = "1 = 1";
+
+	foreach ($condicoes as $coluna=>$valor) {
+		$where = $where . " AND $coluna = '$valor'";
+	}
+
+	$query =  	<<<SQL
+					SELECT * FROM {$tabela}
+					WHERE %s;
+				SQL;
+	$prepare = $wpdb->prepare($query, $where);
+	$resultados = $wpdb->get_results($prepare, OBJECT);
+
+	foreach($resultados as $resultado) {
+		foreach ($condicoes as $coluna=>$valor) {
+			unset($resultado->$coluna);
+		}
+	}
+	
+	return $resultados;
+}
+
+// Aproxima largura em porcentagem para grid de 12 colunas
+function calculaCol($pct)
+{
+	$col = $pct / 100 * 12;
+	$col = (int) round($col, 0);
+
+	if ($col < 1) {
+		$col = 1;
+	}
+
+	if ($col > 12) {
+		$col = 12;
+	}
+	
+	return $col;
 }
 
 // Shortcodes
@@ -89,4 +140,3 @@ require_once "modulo-botao-voltar.php";
 
 return ob_get_clean();
 }
-
