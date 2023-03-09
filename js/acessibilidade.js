36b127f4c1562a3e131560aa80ebc07dad7d3e73
vgiuliani11 <vinicius.giuliani@gmail.com>
2023-03-09 19:15:16 -0300

    Corrige atribuição da classe do body para o html (document root)

--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -1,5 +1,20 @@
-function aumentarTexto(){
-    const corpo=document.getElementsByTagName('body')[0]
-    corpo.classList.add('tamanho-fonte-115')
+function aumentarTexto() {
+    // Seleciona o elemento root (tag html)
+    const root = document.documentElement;
+
+    // APÓS 1º CLIQUE
+    root.className = 'tamanho-fonte-115';
+
+    // TO-DO: APÓS 2º CLIQUE EM DIANTE
+
 }
 
+function diminuirTexto() {
+    // Seleciona o elemento root (tag html)
+    const root = document.documentElement;
+
+    // TO-DO: DIMINUIR TEXTO
+
+}
+
+// TO-DO: GUARDAR ESTADO DO TAMANHO DE TEXTO
