5d78755dba9f83392ccc5a45f810935d253f82d5
marcelgn1800 <80598807+marcelgn1800@users.noreply.github.com>
2023-03-13 13:22:01 -0300

    Foi adicionado tamanho-fonte nas functions aumentarTexto e diminuirTexto

--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -4,8 +4,13 @@ function aumentarTexto() {
 
     // APÓS 1º CLIQUE
     root.className = 'tamanho-fonte-115';
+    root.className = 'tamanho-fonte-130';
+    root.className = 'tamanho-fonte-150';
+    root.className = 'tamanho-fonte-180';
 
     // TO-DO: APÓS 2º CLIQUE EM DIANTE
+
+    document.documentElement.fontSize=tamanho+"px";
 
 }
 
@@ -13,8 +18,30 @@ function diminuirTexto() {
     // Seleciona o elemento root (tag html)
     const root = document.documentElement;
 
+    root.className = 'tamanho-fonte-90';
+    root.className = 'tamanho-fonte-80';
+    root.className = 'tamanho-fonte-70';
+    root.className = 'tamanho-fonte-60';
+
     // TO-DO: DIMINUIR TEXTO
+
+    document.documentElement.fontSize=tamanho-"px";
 
 }
 
 // TO-DO: GUARDAR ESTADO DO TAMANHO DE TEXTO
+
+/*tamanho = 11.5;
+function diminuirTexto(){
+    tamanho--;
+    document.documentElement.fontSize=tamanho+"px";
+}
+
+function aumentarTexto(){
+    tamanho--;
+    document.documentElement.fontSize=tamanho+"px";
+}
+*/
+
+
+
