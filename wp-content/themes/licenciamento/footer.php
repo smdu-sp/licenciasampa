921af72202b0f15c5e534e9810662f160f2f99c7
vgiuliani11 <vinicius.giuliani@gmail.com>
2023-03-28 17:56:22 -0300

    Init footer

--- wp-content/themes/licenciamento/footer.php
+++ wp-content/themes/licenciamento/footer.php
@@ -7,15 +7,36 @@
 
 ?>
 
-<?php
-
-if ( function_exists( 'dslc_hf_get_footer' ) ) {
-	echo dslc_hf_get_footer();
-}
-
-?>
+<footer id="rodape">
+	<?php include_once 'footer/nav.php' ?>
+</footer>
 
 <?php wp_footer(); ?>
 
+<style>
+	#rodape {
+		padding-bottom:90px;
+		padding-top:90px;
+		background-color:#395aad;
+	}
+
+	.footer-nav {
+		padding-left: 0;
+	}
+
+	.footer-redes-sociais {
+		display: block;
+		width: 100px;
+		padding-left: 0;
+	}
+
+	.footer-redes-sociais li {
+		display: inline-block;
+		margin-right: 16px;
+	}
+
+</style>
+
 </body>
+
 </html>
